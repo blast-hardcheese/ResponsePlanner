88317afa6a9bcead0a6488303f1d62990586ce78
Devon Stewart <blast@hardchee.se>
2014-06-29 01:02:30 -0700

    Breaking out ArcGisAPIHandler

--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
 ///<reference path="definitions/jquery/jquery.d.ts" />
 ///<reference path="definitions/googlemaps/google.maps.d.ts" />
 ///<reference path="definitions/toastr/toastr.d.ts" />
+///<reference path="definitions/geojson/geojson.d.ts" />
 
 module ResponsePlanner {
-    class APIHandler {
+    interface APIHandler {
 
     }
 
+    class ArcGisAPIHandler implements APIHandler {
+
+        private base = "http://gis.rctlma.org/arcgis/rest/services/Public";
+        serviceName: string = null;
+        serviceIndex: number = null;
+
+        constructor(serviceName: string, serviceIndex) {
+            this.serviceName = serviceName;
+            this.serviceIndex = serviceIndex;
+        }
+    }
 
     export class App {
         map: google.maps.Map = null;
@@ -21,7 +33,7 @@ module ResponsePlanner {
                 center: new google.maps.LatLng(0, 0),
             };
 
-            this.api = new APIHandler();
+            this.api = new ArcGisAPIHandler("FeatureServer");
 
             this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
             this.location_init();
